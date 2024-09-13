e47da95f04ed0f5c67ddac81814fed991d6ec0b9
Akmal <mamatovakmaljon@gmail.com>
2024-09-13 11:55:41 +0500

    Commit1

---
+++ app/src/main/java/uz/market/market/activity/IntroActivity.kt
@@ -0,0 +1,25 @@
+package uz.market.market.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import uz.market.market.MainActivity
+import uz.market.market.R
+import uz.market.market.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@IntroActivity,MainActivity::class.java))
+        }
+
+    }
+}
