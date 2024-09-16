d5ca36280800c37288c2655e94f486401ae19e85
Akmal <mamatovakmaljon@gmail.com>
2024-09-16 16:23:16 +0500

    Commit10 ok

---
+++ app/src/main/java/uz/market/market/Adapter/CartAdapter.kt
@@ -0,0 +1,67 @@
+package uz.market.market.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.request.RequestOptions
+import com.example.project1762.Helper.ChangeNumberItemsListener
+import com.example.project1762.Helper.ManagmentCart
+import uz.market.market.Model.ItemsModel
+import uz.market.market.databinding.ViewholderCartBinding
+
+class CartAdapter(
+    private val listItemSlected: ArrayList<ItemsModel>,
+    context: Context,
+    var changeNumberItemsListener: ChangeNumberItemsListener? = null
+
+) : RecyclerView.Adapter<CartAdapter.Viewholder>() {
+    class Viewholder(val binding: ViewholderCartBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    private val managmentCart = ManagmentCart(context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartAdapter.Viewholder {
+        val binding =
+            ViewholderCartBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Viewholder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CartAdapter.Viewholder, position: Int) {
+        val item = listItemSlected[position]
+
+        holder.binding.titleTxt.text = item.title
+        holder.binding.feeEachItem.text = "$${item.price}"
+        holder.binding.totalEachItem.text = "$${Math.round(item.numberInCart * item.price)}"
+        holder.binding.numberItemTxt.text = item.numberInCart.toString()
+
+
+        Glide.with(holder.itemView.context)
+            .load(item.picUrl[0])
+            .apply(RequestOptions().transform(CenterCrop()))
+            .into(holder.binding.pic)
+
+        holder.binding.plusCartBtn.setOnClickListener {
+            managmentCart.plusItem(listItemSlected, position, object : ChangeNumberItemsListener {
+                override fun onChanged() {
+                    notifyDataSetChanged()
+                    changeNumberItemsListener?.onChanged()
+                }
+
+            })
+        }
+        holder.binding.minusCartBtn.setOnClickListener {
+            managmentCart.minusItem(listItemSlected, position, object : ChangeNumberItemsListener {
+                override fun onChanged() {
+                    notifyDataSetChanged()
+                    changeNumberItemsListener?.onChanged()
+                }
+
+            })
+        }
+    }
+
+    override fun getItemCount(): Int = listItemSlected.size
+}
